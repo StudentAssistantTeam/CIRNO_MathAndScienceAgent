6a0da49d7251be1781ef2c1a225bc789be8dfd43
NewtonZhan <zhanyunze0601@gmail.com>
2026-02-05 15:34:24 +0800

    dev(gitignore): Adding gitignore

---
+++ cirno_math_and_science_agent/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# Settings
+class Settings(BaseSettings):
+    # a2a config
+    a2a_port:str = ""
+    # LLM setting
+    llm_model_name:str = ""
+    llm_base_url:str = ""
+    llm_api_key:str = ""
+
+    model_config = SettingsConfigDict(
+        env_file="cirno_math_and_science_agent.env",
+        env_file_encoding="utf-8",
+        extra="allow",
+    )
+
+settings = Settings()
