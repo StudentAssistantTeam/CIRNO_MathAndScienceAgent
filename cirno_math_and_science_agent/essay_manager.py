82feab022f25b9671d830ed3672d8ba6168025cf
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-01 10:05:21 +0800

    feat(Tool): Adding the info fetching in academics researcher tool

--- cirno_math_and_science_agent/essay_manager.py
+++ cirno_math_and_science_agent/essay_manager.py
@@ -2,6 +2,7 @@ from cirno_math_and_science_agent.config import settings
 import logging
 import fitz
 import re
+import httpx
 
 logger = logging.getLogger("essay_manager")
 # Load PDF
@@ -20,3 +21,53 @@ def get_work_id(url: str):
     if match:
         work_id = match.group(1)
         return work_id
+# Essay info searching
+async def get_essay_info(query: str, number: int):
+    # Request params
+    params = {
+        "api_key": settings.openalex_api,
+        "search": query,
+        "page":1,
+        "per-page":number,
+        "filter":"has_content.pdf:true"
+    }
+    url = "https://api.openalex.org/works"
+    try:
+        # Result storage
+        results = []
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url=url, params=params, timeout=60)
+            if response.status_code==200:
+                logger.info("Request successful")
+                response_json = response.json()
+                result = response_json["results"]
+                # Data processing
+                for i in result:
+                    processed_result = {}
+                    # Find id and other info for the result
+                    if "id" in i.keys():
+                        processed_result["id"] = get_work_id(i["id"])
+                    if "doi" in i.keys():
+                        processed_result["doi"] = i["doi"]
+                    if "title" in i.keys():
+                        processed_result["title"] = i["title"]
+                    # Add to the results
+                    results.append(processed_result)
+                return {
+                    "success": True,
+                    "results": results
+                }
+            else:
+                logger.info(f"request failed due to {response.status_code}")
+                # Process the failed requests due to http problems
+                return {
+                    "success": False,
+                    "error": f"search failed due to {response.content}"
+                }
+    except Exception as e:
+        # Error handling
+        logger.error(f"Search failed due to {e}")
+        return {
+            "success": False,
+            "error": f"search failed due to {e}"
+        }
