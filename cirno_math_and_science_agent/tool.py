5369ac2bd11722e04d0d80fb5f6980b42f5e19a1
NewtonZhan <zhanyunze0601@gmail.com>
2026-04-01 20:29:15 +0800

    feat(tools): Add testing function

--- cirno_math_and_science_agent/tool.py
+++ cirno_math_and_science_agent/tool.py
@@ -93,7 +93,60 @@ async def academics_searcher(query: str) -> str:
         return f"Essay searching failed due to {e}"
 
 
+# Testing
+async def academics_searcher_test(query: str) -> str:
+    try:
+        logger.info("Start searching in research essay database...")
+        # Essay info list
+        essay_info = await get_essay_info(query=query, number=5)
+        # Process essay info
+        succeed_list = []
+        # Process essay list
+        if (essay_info["success"]):
+            for i in essay_info["results"]:
+                if "id" in i.keys() and "title" in i.keys():
+                    tmp = {}
+                    tmp["id"] = i["id"]
+                    tmp["title"] = i["title"]
+                    if "doi" in i.keys():
+                        tmp["doi"] = i["doi"]
+                    else:
+                        tmp["doi"] = "N/A"
+                    succeed_list.append(tmp)
+        # Processed list
+        processed_list = []
+
+        async def get_essay_summary(info_dict):
+            processor = essay_processor(doi=info_dict["doi"], id=info_dict["id"], essay_name=info_dict["title"])
+            # Download the essay file
+            await processor.download()
+            summary = await processor.get_summary()
+            if summary["success"]:
+                tmp_dict = {}
+                tmp_dict["doi"] = summary["doi"]
+                tmp_dict["title"] = summary["essay_name"]
+                tmp_dict["summary"] = summary["summary"]
+                processed_list.append(tmp_dict)
+
+        # Gather the tasks and execute
+        tasks = [get_essay_summary(i) for i in succeed_list]
+        await asyncio.gather(*tasks)
+        # Return
+        return "result of searching for essays: " + "".join(
+            [
+                f"\n\nResult No.{idx + 1}:\n\nTitle: {result['title']}\n\ndoi: {result["doi"]}\n\nShort Summary: \n\n{result["summary"]}"
+                for idx, result in enumerate(processed_list)
+            ]
+        )
+    except Exception as e:
+        return f"Essay searching failed due to {e}"
+
+
 # Get final answer.
 @tool("final_answer", description=final_answer_description)
 async def final_answer() -> None:
     return None
+
+
+if __name__ == "__main__":
+    print(asyncio.run(academics_searcher_test("Application of quantum physics")))
